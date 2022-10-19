01f8cf70f5bafe4b9694d8da5f618fa3553a9534
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-10-19 17:44:34 -0300

    feat: added first subscirse

--- Camera Filter/FilterCollectionViewController.swift
+++ Camera Filter/FilterCollectionViewController.swift
@@ -7,16 +7,23 @@
 
 import UIKit
 import Photos
+import RxSwift
 
 private let reuseIdentifier = "Cell"
 
 class FilterCollectionViewController: UICollectionViewController {
 	
 	var collectionAssets = [PHAsset]()
+	var imagePublishSubject = PublishSubject<UIImage>()
+	var imageObservable: Observable<UIImage> {
+		return imagePublishSubject.asObservable()
+	}
+	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		populatePhotos()
+		
 		
 		self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 		
@@ -26,6 +33,9 @@ class FilterCollectionViewController: UICollectionViewController {
 		//weak self e para garantir qeu nao ocorra memoria leak pois
 		//fetchAssets e assincrono
 		PHPhotoLibrary.requestAuthorization {[weak self] (status) in
+			print("entrei aqui")
+			
+			
 			if status == .authorized  {
 				let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
 				
@@ -39,22 +49,15 @@ class FilterCollectionViewController: UICollectionViewController {
 				
 				DispatchQueue.main.async {
 					self?.collectionView.reloadData()
+					
 				}
 				
 			}
+			
 		}
-		
 	}
 	
-	/*
-	 // MARK: - Navigation
-	 
-	 // In a storyboard-based application, you will often want to do a little preparation before navigation
-	 override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-	 // Get the new view controller using [segue destinationViewController].
-	 // Pass the selected object to the new view controller.
-	 }
-	 */
+	
 	
 	// MARK: UICollectionViewDataSource
 	
@@ -68,52 +71,43 @@ class FilterCollectionViewController: UICollectionViewController {
 	}
 	
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		guard	let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? PhotosCollectionViewCell  else { fatalError("PhotosCollectionViewCell dont existe") }
-		
+		guard	let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? PhotosCollectionViewCell else {
+			fatalError("PhotosCollectionViewCell dont exists")
+		}
 		let asset = collectionAssets[indexPath.row]
 		let manager = PHImageManager()
 		
 		//assincrono request por isso dispatchQueue
 		//estou fazendo a requisicao de cada foto e colocando na img photo
-		manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+		manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFill, options: nil) { image, _ in
 			
 			DispatchQueue.main.async {
 				cell.imgPhoto.image = image
 			}
+			
 		}
-		
 		return cell
 	}
 	
-	// MARK: UICollectionViewDelegate
-	
-	/*
-	 // Uncomment this method to specify if the specified item should be highlighted during tracking
-	 override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-	 return true
-	 }
-	 */
-	
-	/*
-	 // Uncomment this method to specify if the specified item should be selected
-	 override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-	 return true
-	 }
-	 */
-	
-	/*
-	 // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-	 override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-	 return false
-	 }
-	 
-	 override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-	 return false
-	 }
-	 
-	 override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-	 
-	 }
-	 */
+	override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		let asset = collectionAssets[indexPath.row]
+		PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFill, options: nil) {[weak self] image,info in
+			
+			guard let info = info else {return }
+			
+			let isDegredImage = info["PHImageResultIsDegradeKey"] as! Bool
+			
+			if !isDegredImage  {
+				
+				if let image = image {
+					self?.imagePublishSubject.onNext(image)
+					self?.dismiss(animated: true, completion: nil)
+					
+				}
+				
+			}
+			
+		} }
 	
 }
+
